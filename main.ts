719d7eb7d2e36937478c78b092565cc038b6aaad
Nacho131104 <nacholopezamat@gmail.com>
2025-06-30 12:06:41 +0200

    commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+//import { typeDefs } from "./schema.ts";
+import { MongoClient } from "mongodb";
+//import { RestaurantModel } from "./types.ts";
+import { startStandaloneServer } from "@apollo/server/standalone";
+//import { resolvers } from "./resolvers.ts";
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+
+if (!MONGO_URL) {
+  throw new Error("MONGO_URL is not defined");
+}
+
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
+
+const mongoDB = mongoClient.db("BaseFInal");
+const ResturantsCollection =
+  mongoDB.collection<>("restaurantes");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  context: async () => ({ ResturantsCollection }),
+});
+
+console.info(`Server ready at ${url}`);
